41dd925310aba944c6ea62c6a1d2e35920bed2d0
PeterRutkowski <piotrrutkowski97@gmail.com>
2021-04-18 01:31:47 +0200

    code for perturbations ready

--- data_converter.py
+++ data_converter.py
@@ -1,8 +1,10 @@
-import cv2
 import data_split
 from tqdm import tqdm
 from PIL import Image
 import numpy as np
+import salt_pepper_noise
+import gaussian_noise
+from skimage.filters import gaussian
 
 
 class DataConverter:
@@ -14,23 +16,24 @@ class DataConverter:
         ds = data_split.DataSplit()
         ds.save_split(path, save_path, test_size, random_state)
     
-    def convert(self, paths, noise='none'):
-        images = []
-        
-        for i in tqdm(range(len(paths)), desc='[Converting images]'.format(noise)):
+    def perturb(self, paths, set_type, save_path, blur, mode):
+        images = list()
+        predictions = list()
+        for i in tqdm(range(len(paths)), desc='[{} {}]'.format(blur, mode)):
             img = np.asarray(Image.open(paths[i]))
-            if noise == 'gaussian':
-                img = cv2.GaussianBlur(img, (5, 5), 0)
-            images.append(self.model.predict(Image.fromarray(img.astype('uint8'), 'RGB')))
-        
-        return np.asarray(images)
-    
-    def convert_split(self, path='data/in10_split.npz', save_path='data/in10_split_converted'):
-        loaded = np.load(path)
-        
-        np.savez(save_path,
-                 x_test_none=self.convert(loaded['paths_test']),
-                 #x_test_gaussian=self.convert(loaded['paths_test'], 'gaussian'),
-                 x_train=self.convert(loaded['paths_train']),
-                 y_train=loaded['labels_train'],
-                 y_test=loaded['labels_test'])
+            if blur == 'salt_pepper_noise':
+                img = salt_pepper_noise.SaltPepperNoise().noise(img, mode)
+            elif blur == 'gaussian_noise':
+                img = gaussian_noise.GaussianNoise().noise(img, mode)
+            elif blur == 'gaussian_blur':
+                img = gaussian(img, sigma=mode, preserve_range=True)
+
+            img = img.astype('uint8')
+
+            images.append(img)
+            predictions.append(self.model.predict(Image.fromarray(img, 'RGB')))
+
+        np.savez_compressed('{}/images_{}_{}_{}'.format(save_path, set_type, blur, mode),
+                            data=np.asarray(images))
+        np.savez_compressed('{}/{}_{}_{}'.format(save_path, set_type, blur, mode),
+                            data=np.asarray(predictions))
---
+++ pipeline_data.py
@@ -0,0 +1,45 @@
+import data_converter
+import vgg19bn
+import numpy as np
+
+dc = data_converter.DataConverter(vgg19bn.VGG19bn(layers=[60]))
+
+dc.create_split(path='data', save_path='pipeline_data/in10_split')
+
+data_paths = np.load('pipeline_data/in10_split')
+
+# labels
+
+np.savez_compressed('pipeline_data/y_train', data=data_paths['labels_train'])
+np.savez_compressed('pipeline_data/y_test', data=data_paths['labels_test'])
+
+# training set
+
+dc.perturb(paths=data_paths['paths_train'],
+           set_type='x_train',
+           save_path='pipeline_data',
+           blur='none',
+           mode='none')
+
+# testing set
+
+for thr in np.arange(0.03, 0.36, 0.03):
+    dc.perturb(paths=data_paths['paths_test'],
+               set_type='x_test',
+               save_path='pipeline_data',
+               blur='salt_pepper_noise',
+               mode=thr)
+
+for sd in np.arange(10, 120, 10):
+    dc.perturb(paths=data_paths['paths_test'],
+               set_type='x_test',
+               save_path='pipeline_data',
+               blur='gaussian_noise',
+               mode=sd)
+
+for sigma in np.arange(0.5, 6, 0.5):
+    dc.perturb(paths=data_paths['paths_test'],
+               set_type='x_test',
+               save_path='pipeline_data',
+               blur='gaussian_blur',
+               mode=sigma)
