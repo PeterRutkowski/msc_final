c6c086add2d2cf64b72f18d03cb0db5feaf87afe
Piotr Rutkowski <pr386097@chuck.mimuw.edu.pl>
2021-04-26 01:31:46 +0200

    models done

--- pipeline_brp.py
+++ pipeline_brp.py
@@ -2,16 +2,16 @@ import numpy as np
 from sklearn.svm import SVC
 import multiprocessing as mp
 import joblib
+from os import walk
 
 
 def component_model(train_data):
     x, y = train_data[0], train_data[1]
     clf = SVC(kernel='poly', degree=2)
     clf.fit(x, y)
-
+ 
     joblib.dump(clf, 'pipeline_data/comp120_dbscan200/model_{}'.format(train_data[2]),
                 compress='lzma')
-
     return {test_set: clf.predict(np.load('pipeline_data/{}.npz'.format(test_set),
                                           allow_pickle=True)['data'])
             for test_set in ['x_test_none_none',
@@ -53,10 +53,22 @@ def component_model(train_data):
 x_train = np.load('pipeline_data/x_train_none_none.npz', allow_pickle=True)['data']
 y_train = np.load('pipeline_data/comp120_dbscan200/rep_x_train.npz', allow_pickle=True)['data']
 
-pool = mp.Pool(int(mp.cpu_count()*0.6))
-dicts = pool.map(component_model, [[x_train, y_train[:, i], i]
-                                   for i in range(y_train.shape[1])])
+_, _, filenames = next(walk('pipeline_data/comp120_dbscan200'))
 
+done = list()
+for filename in filenames:
+    if filename.startswith('model'):
+        done.append(int(filename[6:]))
+
+todo = [n for n in range(y_train.shape[1])]
+
+for n in done:
+    todo.remove(n)
+
+print(todo)
+pool = mp.Pool(75)
+dicts = pool.map(component_model, [[x_train, y_train[:, i], i] for i in todo])
+'''
 for test_set in ['x_test_none_none',
                  'x_test_gaussian_blur_0.5',
                  'x_test_gaussian_blur_1.0',
@@ -93,3 +105,4 @@ for test_set in ['x_test_none_none',
                  'x_test_salt_pepper_noise_0.33']:
     np.savez('pipeline_data/comp120_dbscan200/rep_{}'.format(test_set),
              data=np.asarray([d[test_set] for d in dicts]).T)
+'''
