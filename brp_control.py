e83b1f23bd86c9e23a237e9a5a784729b2af8052
PeterRutkowski <piotrrutkowski97@gmail.com>
2021-04-29 02:36:54 +0200

    pipeline - mappers

--- brp_control.py
+++ brp_control.py
@@ -3,44 +3,6 @@ import joblib
 import multiprocessing as mp
 from os import walk
 
-'''for test_set in ['x_test_none_none',
-                 'x_test_gaussian_blur_0.5',
-                 'x_test_gaussian_blur_1.0',
-                 'x_test_gaussian_blur_1.5',
-                 'x_test_gaussian_blur_2.0',
-                 'x_test_gaussian_blur_2.5',
-                 'x_test_gaussian_blur_3.0',
-                 'x_test_gaussian_blur_3.5',
-                 'x_test_gaussian_blur_4.0',
-                 'x_test_gaussian_blur_4.5',
-                 'x_test_gaussian_blur_5.0',
-                 'x_test_gaussian_blur_5.5',
-                 'x_test_gaussian_noise_10',
-                 'x_test_gaussian_noise_20',
-                 'x_test_gaussian_noise_30',
-                 'x_test_gaussian_noise_40',
-                 'x_test_gaussian_noise_50',
-                 'x_test_gaussian_noise_60',
-                 'x_test_gaussian_noise_70',
-                 'x_test_gaussian_noise_80',
-                 'x_test_gaussian_noise_90',
-                 'x_test_gaussian_noise_100',
-                 'x_test_gaussian_noise_110',
-                 'x_test_salt_pepper_noise_0.03',
-                 'x_test_salt_pepper_noise_0.06',
-                 'x_test_salt_pepper_noise_0.09',
-                 'x_test_salt_pepper_noise_0.12',
-                 'x_test_salt_pepper_noise_0.15',
-                 'x_test_salt_pepper_noise_0.18',
-                 'x_test_salt_pepper_noise_0.21',
-                 'x_test_salt_pepper_noise_0.24',
-                 'x_test_salt_pepper_noise_0.27',
-                 'x_test_salt_pepper_noise_0.30',
-                 'x_test_salt_pepper_noise_0.33']:
-    print(test_set,
-          np.load('pipeline_data/comp120_dbscan200/rep_{}.npz'.format(test_set),
-                  allow_pickle=True)['data'].shape)'''
-
 
 def bad(input_data):
     ind, exp = input_data[0], input_data[1]
@@ -52,7 +14,7 @@ def bad(input_data):
 
 
 experiments = list()
-for n_components in [60, 90, 120]:
+for n_components in [120]:
     for epsilon in [150, 100]:
         for n_intervals in [4, 7, 10]:
             experiments.append('pca{}_eps{}_int{}'.format(n_components, epsilon, n_intervals))
