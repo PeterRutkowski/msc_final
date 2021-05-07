4cf398f1879be2fa7ba561e3dce21f8c01474404
PeterRutkowski <piotrrutkowski97@gmail.com>
2021-05-07 11:52:28 +0200

    up

--- pipeline_data.py
+++ pipeline_data.py
@@ -15,7 +15,8 @@ np.savez_compressed('data/y_test', data=data_paths['labels_test'])
 
 # training set
 
-dc.perturb(paths=data_paths['paths_train'],
+dc.perturb(model=vgg,
+           paths=data_paths['paths_train'],
            set_type='x_train',
            save_path='data',
            blur='none',
@@ -23,28 +24,32 @@ dc.perturb(paths=data_paths['paths_train'],
 
 # testing set
 
-dc.perturb(paths=data_paths['paths_test'],
+dc.perturb(model=vgg,
+           paths=data_paths['paths_test'],
            set_type='x_test',
            save_path='data',
            blur='none',
            mode='none')
 
 '''for thr in np.arange(0.03, 0.36, 0.03):
-    dc.perturb(paths=data_paths['paths_test'],
+    dc.perturb(model=vgg,
+               paths=data_paths['paths_test'],
                set_type='x_test',
                save_path='data',
                blur='salt_pepper_noise',
                mode=thr)'''
 
 '''for sd in np.arange(10, 120, 10):
-    dc.perturb(paths=data_paths['paths_test'],
+    dc.perturb(model=vgg,
+               paths=data_paths['paths_test'],
                set_type='x_test',
                save_path='data',
                blur='gaussian_noise',
                mode=sd)'''
 
 for sigma in np.arange(0.5, 6, 0.5):
-    dc.perturb(paths=data_paths['paths_test'],
+    dc.perturb(model=vgg,
+               paths=data_paths['paths_test'],
                set_type='x_test',
                save_path='data',
                blur='gaussian_blur',
