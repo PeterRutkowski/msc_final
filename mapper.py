c26091b2566d2018ea3144744020cdd6aff2b1ec
PeterRutkowski <piotrrutkowski97@gmail.com>
2021-05-08 18:54:53 +0200

    up

--- mapper.py
+++ mapper.py
@@ -8,68 +8,63 @@ from sklearn.pipeline import Pipeline
 from gtda.mapper import *
 
 
-class Mapper:
-    def __init__(self):
-        pass
+def fit(x, projector, clusterer, n_components, n_intervals, experiment_name,
+        overlap_frac=0.33, kind='uniform'):
+    """Trains Mapper algorithm on x.
 
-    @staticmethod
-    def fit(x, projector, clusterer, n_components, n_intervals, experiment_name,
-            overlap_frac=0.33, kind='uniform'):
-        """Trains Mapper algorithm on x.
+    :param x: Images.
+    :type x: numpy.ndarray
+    :param projector: Model projecting images onto latent space.
+    :param clusterer: Model clustering images.
+    :param n_components: Number of latent space dimensions.
+    :type n_components: int
+    :param n_intervals: Number of intervals covering each latent
+    space component.
+    :type n_intervals: int
+    :param overlap_frac: Parameter defining much intervals can
+    overlap.
+    :type overlap_frac: float
+    """
 
-        :param x: Images.
-        :type x: numpy.ndarray
-        :param projector: Model projecting images onto latent space.
-        :param clusterer: Model clustering images.
-        :param n_components: Number of latent space dimensions.
-        :type n_components: int
-        :param n_intervals: Number of intervals covering each latent
-        space component.
-        :type n_intervals: int
-        :param overlap_frac: Parameter defining much intervals can
-        overlap.
-        :type overlap_frac: float
-        """
+    projector = projector.fit(x)
+    mapper_pipes = []
 
-        projector = projector.fit(x)
-        mapper_pipes = []
-        
-        for i in range(n_components):
-            mapper_pipe = make_mapper_pipeline(scaler=None,
-                                               filter_func=Pipeline(
-                                                   steps=[('projector', projector),
-                                                          ('proj', Projection(columns=i))],
-                                                   verbose=0),
-                                               cover=OneDimensionalCover(kind=kind,
-                                                                         n_intervals=n_intervals,
-                                                                         overlap_frac=overlap_frac),
-                                               clusterer=clusterer,
-                                               verbose=False,
-                                               n_jobs=1)
-            mapper_pipe.set_params(filter_func__proj__columns=i)
-            mapper_pipes.append(('comp{}'.format(i+1), mapper_pipe))
-        
-        latent_projector = latent_space.LatentSpace([projector], 'latent_space')
-        
-        graphs = Parallel(n_jobs=int(0.8*mp.cpu_count()), prefer="threads", verbose=0)(
-            delayed(mapper_pipe[1].fit_transform)(x) for mapper_pipe in mapper_pipes)
-        
-        x_proj = projector.transform(x)
-        covers = []
-        for i in range(n_components):
-            odc = OneDimensionalCover(kind=kind,
-                                      n_intervals=n_intervals,
-                                      overlap_frac=overlap_frac).fit(x_proj[:, i])
-            covers.append([(odc.left_limits_[j], odc.right_limits_[j])
-                           for j in range(n_intervals)])
+    for i in range(n_components):
+        mapper_pipe = make_mapper_pipeline(scaler=None,
+                                           filter_func=Pipeline(
+                                               steps=[('projector', projector),
+                                                      ('proj', Projection(columns=i))],
+                                               verbose=1),
+                                           cover=OneDimensionalCover(kind=kind,
+                                                                     n_intervals=n_intervals,
+                                                                     overlap_frac=overlap_frac),
+                                           clusterer=clusterer,
+                                           verbose=False,
+                                           n_jobs=1)
+        mapper_pipe.set_params(filter_func__proj__columns=i)
+        mapper_pipes.append(('comp{}'.format(i+1), mapper_pipe))
 
-        with open('pipeline_data/{}/mapper'.format(experiment_name), 'wb') as f:
-            pickle.dump((latent_projector, graphs, covers), f)
+    latent_projector = latent_space.LatentSpace([projector], 'latent_space')
 
-    @staticmethod
-    def get_representations(x, graphs, experiment_name):
-        b = binarizer.Binarizer()
-        x_rep = b.binarize(x, graphs)
-        print(x_rep.shape)
-        np.savez_compressed('pipeline_data/{}/bin_rep_x_train.npz'.format(experiment_name),
-                            data=x_rep)
+    graphs = Parallel(n_jobs=int(0.8*mp.cpu_count()), prefer="threads", verbose=1)(
+        delayed(mapper_pipe[1].fit_transform)(x) for mapper_pipe in mapper_pipes)
+
+    x_proj = projector.transform(x)
+    covers = []
+    for i in range(n_components):
+        odc = OneDimensionalCover(kind=kind,
+                                  n_intervals=n_intervals,
+                                  overlap_frac=overlap_frac).fit(x_proj[:, i])
+        covers.append([(odc.left_limits_[j], odc.right_limits_[j])
+                       for j in range(n_intervals)])
+
+    with open('pipeline_data/{}/mapper'.format(experiment_name), 'wb') as f:
+        pickle.dump((latent_projector, graphs, covers), f)
+
+
+def get_representations(x, graphs, experiment_name):
+    b = binarizer.Binarizer()
+    x_rep = b.binarize(x, graphs)
+    print(x_rep.shape)
+    np.savez_compressed('pipeline_data/{}/bin_rep_x_train.npz'.format(experiment_name),
+                        data=x_rep)
--- pipeline_mapper.py
+++ pipeline_mapper.py
@@ -13,19 +13,20 @@ def mapper_training(n_components, epsilon, n_intervals):
     os.mkdir('data/{}'.format(dir_name))
     print(dir_name, end=' ')
 
-    clusterer = DBSCAN(eps=epsilon, min_samples=1)
-    projector = my_pca.MyPCA(n_components=n_components)
-
-    x_train = np.load('data/x_train_none_none.npz', allow_pickle=True)['data']
-
-    m = mapper.Mapper()
-    m.fit(x_train, projector=projector, clusterer=clusterer, n_components=n_components,
-          n_intervals=n_intervals, experiment_name=dir_name, kind='uniform')
+    mapper.fit(np.load('data/x_train_none_none.npz', allow_pickle=True)['data'],
+               projector=my_pca.MyPCA(n_components=n_components),
+               clusterer=DBSCAN(eps=epsilon, min_samples=1),
+               n_components=n_components,
+               n_intervals=n_intervals,
+               experiment_name=dir_name,
+               kind='uniform')
 
     with open('data/{}/mapper'.format(dir_name), 'rb') as f:
         graphs = pickle.load(f)[1]
     print(datetime.now() - start_time)
-    m.get_representations(x_train, graphs, dir_name)
+    mapper.get_representations(np.load('data/x_train_none_none.npz', allow_pickle=True)['data'],
+                               graphs,
+                               dir_name)
 
 
-mapper_training(10, 100, 4)
+mapper_training(10, 300, 4)
